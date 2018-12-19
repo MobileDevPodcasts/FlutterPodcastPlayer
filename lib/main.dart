c4cdb5a18a0b43fd7ed338e302cb624f0b7b81b4
Eugene Saturov <esaturov@gmail.com>
2018-12-19 15:04:54 +0300

    Simple network call to get list of Android podcasts from iTunes Search API

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:podcast_player/network/ServerUrls.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Podcast Player',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Podcast Player"),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          fetchData();
+        }),
+      ),
     );
+  }
+
+  void fetchData() {
+    get(getPodcastsUrl).then((response) {
+      print(response.body);
+    });
   }
 }
---
+++ lib/network/ServerUrls.dart
@@ -0,0 +1,6 @@
+//server urls list
+//we are using iTunes Search API in this project to fetch podcasts
+//https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+
+const String getPodcastsUrl =
+    "https://itunes.apple.com/search?term=Android&entity=podcast";
