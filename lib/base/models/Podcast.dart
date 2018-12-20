be88d2816bcecbf287ec0d8bf68df90544828e18
Eugene Saturov <esaturov@gmail.com>
2018-12-20 12:45:27 +0300

    Podcast list from Json to Dart objects mapping.

--- lib/base/models/Podcast.dart
+++ lib/base/models/Podcast.dart
@@ -4,8 +4,21 @@ class Podcast {
   String _collectionName;
   String _feedUrl;
   String _artworkUrl60;
-  String _trackCount;
+  int _trackCount;
   String _country;
   String _contentAdvisoryRating;
   List<String> _genres = [];
+
+  Podcast(result) {
+    _collectionId = result['collectionId'];
+    _collectionName = result['collectionName'];
+    _feedUrl = result['feedUrl'];
+    _artworkUrl60 = result['artworkUrl60'];
+    _trackCount = result['trackCount'];
+    _country = result['country'];
+    _contentAdvisoryRating = result['contentAdvisoryRating'];
+    for (int i = 0; i < result['genres'].length; i++) {
+      _genres.add(result['genres'][i]);
+    }
+  }
 }
--- lib/base/models/PodcastListReponse.dart
+++ lib/base/models/PodcastListReponse.dart
@@ -4,4 +4,11 @@ import 'package:podcast_player/base/models/Podcast.dart';
 class PodcastListResponse {
   int _resultCount;
   List<Podcast> _podcasts = [];
+
+  PodcastListResponse.fromJson(Map<String, dynamic> parsedJson) {
+    _resultCount = parsedJson['resultCount'];
+    for (int i = 0; i < parsedJson['results'].length; i++) {
+      _podcasts.add(Podcast(parsedJson['results'][i]));
+    }
+  }
 }
